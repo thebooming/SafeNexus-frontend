c025a2c2f543833efbe79c9a756215e7cd3503f2
SharkNeko <125949438+SharkNeko@users.noreply.github.com>
2024-12-13 10:55:46 +0800

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>Safe Nexus</h1>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
